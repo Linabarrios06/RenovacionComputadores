9357f6f3aaeefe610a7d7ea170d40fb59d4673f1
Linabarrios06 <129072393+Linabarrios06@users.noreply.github.com>
2023-11-21 20:58:52 -0500

    lito

--- clases.py
+++ clases.py
@@ -15,11 +15,11 @@ class GestorComputadores:
     
     def agregarComputador(self):
         computador = {} #Datos a pedir al usuario 
-        computador["Ambiente"] = input("¿A qué ambiente pertenece?: ")
-        computador["ID"] = int(input("Digite el ID del computador: "))
-        computador["Cargador"] = input("¿El computador tiene CARGADOR (si)/(no)?: ")
-        computador["Mouse"] = input("¿El computador tiene MOUSE (si)/(no)?: ")
-        computador["Novedad"] = input("¿Tiene alguna NOVEDAD el computador?: ")
+        computador["ambiente"] = input("¿A que ambiente pertenece?: ")
+        computador["id"] = int(input("Digite el ID del computador: "))
+        computador["cargador"] = input("¿El computador tiene CARGADOR (si)/(no)?: ")
+        computador["mouse"] = input("¿El computador tiene MOUSE (si)/(no)?: ")
+        computador["novedad"] = input("¿Tiene alguna NOVEDAD el computador?: ")
         self.computadores.append(computador)
                                             #Datos guardados
     def guardarDatos(self):
@@ -39,7 +39,7 @@ class GestorComputadores:
                 comp["Novedad"] = input("Ingrese la nueva NOVEDAD para el computador: ")
                 print("Computador modificado correctamente.")
                 return
-        print("No se encontró ningún computador con ese ID.") #Si no se encuentra el id no seguira
+        print("No se encontro ningun computador con ese ID.") #Si no se encuentra el id no seguira
 
     def eliminarComputador(self): #Para eliminar la computadora ingresada
         id_eliminar = int(input("Digite el ID del computador que desea eliminar: "))
@@ -48,19 +48,19 @@ class GestorComputadores:
                 self.computadores.remove(comp)
                 print("Computador eliminado correctamente.")
                 return
-        print("No se encontró ningún computador con ese ID.") #No se va a eliminar  nada porque no se encontro el id
+        print("No se encontro ningun computador con ese ID.") #No se va a eliminar  nada porque no se encontro el id
 
     def mostrarLista(self):
         print("Lista de COMPUTADORES:\n", json.dumps(self.computadores, indent=2)) #Muestra los datos que se ingresaron 
 
 
-# Main
-#OBJETOS
+# Main 
+#OBJETOS                             los llamamos 
 gestor = GestorComputadores()
 
-NumCompu = int(input("¿Cuantos computadores son?: "))
+numCompu = int(input("¿Cuantos computadores son?: "))
 
-for i in range(NumCompu): 
+for i in range(numCompu): 
     gestor.agregarComputador()
 
 # Guardar datos 
@@ -76,4 +76,4 @@ gestor.modificarComputador()
 gestor.eliminarComputador()
 
 # Mostrar la lista actualizada
-gestor.mostrarLista()
+gestor.mostrarLista() 
